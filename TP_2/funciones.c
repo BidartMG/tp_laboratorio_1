03749b8750858f51c8e0ecfab78a92da7b3dfab9
BidartMG <bidartmgb@hotmail.com>
2017-04-28 16:49:25 -0300

    Add files via upload
    TP n° 2 ABM personas

---
+++ TP_2/funciones.c
@@ -0,0 +1,198 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "funciones.h"
+
+/** \brief Presenta al usuario un menu de opciones para que elija la acción a realizar.
+ *  \param No recibe
+ *  \return El valor elegido por el usuario.
+ */
+
+int menuOpciones()
+{
+    int seleccion;
+
+    printf("\n\n        *- Menu de opciones -*\n\n");
+    printf("  * 1-  Agregar una persona\n  * 2-  Borrar una persona\n  * 3-  Imprimir lista ordenada por nombre");
+    printf("\n  * 4-  Imprimir grafico de edades\n  * 5-  Salir\n");
+    printf("\n\n\tSeleccione una de las opciones: ");
+    scanf("%d",&seleccion);
+
+    return seleccion;
+}
+
+/**\brief Inicializa un array con el valor recibido
+ * \param lista Es el array a ser inicializado
+ * \param arrayLenght Indica la logitud del array
+ * \param valor Es el valor que sera cargado en cada posición
+ * \return void
+ */
+void initArray(EPersona lista[],int arrayLenght,int valor)
+{
+    int i;
+
+    for(i=0; i<arrayLenght; i++)
+    {
+        lista[i].estado=valor;
+    }
+}
+
+/**\brief Verifica si el valor recibido contiene solo letras
+ * \param strinG Array con la cadena a ser analizada
+ * \return 1 si contiene solo ' ' y letras y 0 si no lo es
+ */
+ int isOnlyLetters(char strinG[])
+{
+    int i=0;
+
+    while(strinG[i]!='\0')
+    {
+        if((strinG[i]!=' ') && (strinG[i]<'a' || strinG[i]>'z') && (strinG[i]<'A' || strinG[i]>'Z'))
+        {
+             return 0;
+        }
+        i++;
+    }
+    return 1;
+}
+/**\brief Verifica si el valor recibido es numérico
+ * \param strinG Array con la cadena a ser analizada
+ * \return 1 si es númerico y 0 si no lo es
+ */
+int isOnlyNumber(char strinG[])
+{
+    int i=0;
+
+    while(strinG[i]!='\0')
+    {
+        if(strinG[i]<'0' || strinG[i]>'9')
+        {
+            return 0;
+        }
+        i++;
+    }
+    return 1;
+}
+
+/**\brief Solicita un texto al usuario y lo devuelve
+ * \param message Es el mensaje a ser mostrado
+ * \param input Array donde se cargará el texto ingresado
+ * \return void
+ */
+void getString(char message[],char input[])
+{
+    printf("%s",message);
+    scanf("%s",input);
+}
+
+/**\brief Solicita un texto al usuario y lo devuelve
+ * \param message Es el mensaje a ser mostrado
+ * \param input Array donde se cargará el texto ingresado
+ * \return 1 si el texto contiene solo letras
+ */
+int getStringLetters(char message[],char input[])
+{
+    char auxiliar[256];
+
+    getString(message,auxiliar);
+    if(isOnlyLetters(auxiliar))
+    {
+        strcpy(input,auxiliar);
+        return 1;
+    }
+    return 0;
+}
+
+/**\brief Solicita un texto numérico al usuario y lo devuelve
+ * \param mensaje Es el mensaje a ser mostrado
+ * \param input Array donde se cargará el texto ingresado
+ * \return 1 si el texto contiene solo números
+ */
+int getStringNumbers(char message[],char input[])
+{
+    char auxiliar[256];
+
+    getString(message,auxiliar);
+    if(isOnlyNumber(auxiliar))
+    {
+        strcpy(input,auxiliar);
+        return 1;
+    }
+    return 0;
+}
+
+/**
+ * Obtiene el primer indice libre del array.
+ * @param lista el array se pasa como parametro.
+ * @param arrayLenght es la longitud del array
+ * @param valor es el valor a hallar para saber que esta libre
+ * @return el primer indice disponible si lo halla o -1 si no lo encuentra
+ */
+ int obtenerEspacioLibre(EPersona lista[],int arrayLenght,int valor)
+{
+    int i;
+
+    for(i=0;i<arrayLenght;i++)
+    {
+        if(lista[i].estado==valor)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+/**Obtiene el indice que coincide con el dni pasado por parametro.
+ * @param lista el array se pasa como parametro.
+ * @param dni el dni a ser buscado en el array.
+ * @return el indice en donde se encuentra el elemento que coincide con el parametro dni
+ */
+int buscarPorDni(EPersona lista[],int arrayLenght,int dni )
+{
+    int index=-1;
+    int i;
+    for(i=0;i<arrayLenght;i++)
+    {
+        if(lista[i].dni==dni)
+        {
+            index=i;
+            break;
+        }
+    }
+    return index;
+}
+
+/** \brief Compara dos arrays y los ordena alfabeticamente para listarlos
+ *  \param unaPersona Array a ser evaluado para ordenar
+ *  \return void
+ *
+ */
+ void listar(EPersona lista[],int lenght)
+ {
+     int i,j;
+     EPersona auxiliarLista;
+
+     for(i=0; i<lenght-1;i++)
+     {
+         for(j=i+1; j<lenght; j++)
+         {
+             if(lista[j].estado!=1)
+             {
+                 if(strcmp(lista[i].nombre,lista[j].nombre)>0)
+                 {
+                     auxiliarLista=lista[i];
+                     lista[i]=lista[j];
+                     lista[j]=auxiliarLista;
+                 }
+             }
+         }
+     }
+     for(i=0; i<lenght; i++)
+     {
+         if(lista[i].estado==0)
+         {
+             printf("\n%s -- %d -- %d -- \n",lista[i].nombre,lista[i].edad,lista[i].dni);
+         }
+     }
+     printf("\n\n");
+}
---
+++ TP_2/main.c
@@ -0,0 +1,223 @@
+/*****************************************************************************************
+
+* Programa: ABM Personas TP 2
+*
+* Objetivo:
+
+       ** Definir una estructura que represente una persona, con los campos:
+            - Nombre
+            - Edad
+            - DNI
+            - Flag de estado
+       ** Crear un programa que defina un array de 20 items de la estructura creada con un
+        menú de opciones como el siguiente:
+
+            1. Agregar una persona
+            2. Borrar una persona
+            3. Imprimir lista ordenada por nombre
+            4. Imprimir gráfico de edades
+            5. Salir
+* Se utilizará el campo de estado para indicar si el ítem del array está ocupado o no.
+* El gráfico deberá ser un gráfico de barras en donde se agruparán tres barras:
+
+             * Menores de 18
+             * De 19 a 35
+             * Mayores de 35
+
+*
+* Version 0.1 del 28 abril de 2017
+* Autora: Mónica Bidart
+*
+*****************************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "funciones.h"
+
+#define QUANTITY 20
+
+int main()
+{
+     EPersona arrayPeople[QUANTITY];
+     char auxName[50];
+     char auxAge[4];
+     char auxDni[9];
+     int ageAuxiliar;
+     int dniAuxiliar;
+     int option=0,flag=0;
+     int counter1=0,counter2=0,counter3=0;
+     int mayor,i;
+     int freeIndex,indexAux;
+
+
+     initArray(arrayPeople,QUANTITY,-1);
+     while(option!=5)
+     {
+        system("cls");
+        option=menuOpciones();
+        switch(option)
+         {
+         case 1:
+          freeIndex=obtenerEspacioLibre(arrayPeople,QUANTITY,-1);
+          if(freeIndex==-1)
+            {
+            printf("\nNO QUEDA ESPACIO DISPONIBLE!\n");
+            system("pause");
+            break;
+            }
+          printf("\n * AGREGAR UNA PERSONA *\n");
+          if(!getStringLetters("\nIngrese el nombre: ",auxName))
+            {
+            printf("\nEl nombre debe estar compuesto solo por Letras! ");
+            system("pause");
+            break;
+            }
+          if(!getStringNumbers("Ingrese el DNI: ",auxDni))
+            {
+            printf("El DNI debe ser numerico!  ");
+            system("pause");
+            break;
+            }
+          if(strlen(auxDni)<7 || strlen(auxDni)>8)
+            {
+            printf("Error el DNI Debe contener entre 7 y 8 digitos\n");
+            system("pause");
+            break;
+            }
+          dniAuxiliar=atoi(auxDni);
+          if(buscarPorDni(arrayPeople,QUANTITY,dniAuxiliar)!=-1)
+            {
+            printf("\nEl DNI ya existe!\n ");
+            system("pause");
+            break;
+            }
+          if(!getStringNumbers("Ingrese la edad: ",auxAge))
+            {
+            printf("\nLa edad debe ser numerica!");
+            system("pause");
+            break;
+            }
+          ageAuxiliar=atoi(auxAge);
+          if(!(ageAuxiliar>0 && ageAuxiliar<99))
+            {
+            printf("La edad debe estar comprendida entre 0 y 99\n");
+            system("pause");
+            break;
+            }
+          if(ageAuxiliar<=18)
+            {
+            counter1++;
+            }else if(ageAuxiliar>18 && ageAuxiliar<=35)
+                    {
+                    counter2++;
+                    }else
+                        {
+                        counter3++;
+                        }
+          strcpy(arrayPeople[freeIndex].nombre,auxName);
+          arrayPeople[freeIndex].edad=ageAuxiliar;
+          arrayPeople[freeIndex].dni=dniAuxiliar;
+          arrayPeople[freeIndex].estado=0;
+          printf("\n  CARGA EXITOSA  \n");
+          system("pause");
+          break;
+         case 2:
+          system("cls");
+          printf("\n\n** DAR DE BAJA POR DNI **\n");
+          if(!getStringNumbers("\nIngrese el DNI: ",auxDni))
+            {
+            printf("El DNI debe ser numerico!\n ");
+            system("pause");
+            break;
+            }
+          if(strlen(auxDni)<7 || strlen(auxDni)>8)
+            {
+            printf("Error el DNI Debe contener entre 7 y 8 digitos\n");
+            system("pause");
+            break;
+            }
+          dniAuxiliar=atoi(auxDni);
+          indexAux=buscarPorDni(arrayPeople,QUANTITY,dniAuxiliar);
+          if(indexAux==-1)
+            {
+            printf("\n El DNI ingresado no existe!\n");
+            system("pause");
+            break;
+            }
+          if(arrayPeople[indexAux].edad<=18)
+            {
+            counter1--;
+            }else if(arrayPeople[indexAux].edad>35)
+                    {
+                    counter3--;
+                    }else
+                        {
+                        counter2--;
+                        }
+          arrayPeople[indexAux].estado=-1;
+          printf("\nBAJA EXITOSA \n ");
+          system("pause");
+          break;
+         case 3:
+          system("cls");
+          printf("\n\n** IMPRIMIR LISTADO ORDENADO **\n");
+          listar(arrayPeople,QUANTITY);
+          system("pause");
+          break;
+         case 4:
+          system("cls");
+          printf("\n\n\t** GRAFICO DE EDADES **");
+          printf("\n\n\n\n\n\n");
+
+          if(counter1>=counter2 && counter1>=counter3)
+            {
+            mayor=counter1;
+            }
+            else if(counter2>=counter1 && counter2>=counter3)
+                   {
+                    mayor=counter2;
+                   }
+                   else
+                      {
+                      mayor=counter3;
+                      }
+          for(i=mayor; i>0; i--)
+             {
+             if(i<10)
+               {
+               printf("%02d| ",i);
+               }
+             else
+               {
+               printf("%02d| ",i);
+               }
+             if(i<= counter1)
+               {
+             printf(" *");
+               }
+             if(i<= counter2)
+               {
+               flag=1;
+               printf("\t  *");
+               }
+             if(i<= counter3)
+               {
+               if(flag==0)
+                 {
+                 printf("\t\t*");
+                 }
+               if(flag==1)
+                 {
+                 printf("\t*");
+                 }
+               }
+             printf("\n");
+             }
+          printf("\n   <18\t19-35\t>35\n\n");
+          printf("\n    %d\t  %d\t%d\n\n", counter1, counter2, counter3);
+          system("pause");
+          break;
+          }
+     }
+     return 0;
+}
